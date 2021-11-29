20236e2c1fdbbed028db66fcf98e4b6d6a5e9fb6
danielnoo <dannynoo@gmail.com>
2021-11-29 17:08:43 -0500

    basic user login functinality

--- controllers/users.js
+++ controllers/users.js
@@ -2,22 +2,29 @@ const bcrypt = require("bcrypt");
 const usersRouter = require("express").Router();
 const User = require("../models/user");
 
+
 usersRouter.post("/", async (request, response) => {
-  const body = request.body;
+  try {
 
-  const saltRounds = 10;
-  const passwordHash = await bcrypt.hash(body.password, saltRounds);
+    const body = request.body;
 
-  const user = new User({
-    username: body.username,
-    name: body.name,
-    passwordHash,
-    credits: 1000,
-  });
+    const saltRounds = 10;
+    const passwordHash = await bcrypt.hash(body.password, saltRounds);
 
-  const savedUser = await user.save();
+    const user = new User({
+      username: body.username,
+      name: body.name,
+      passwordHash,
+      credits: 1000,
+    });
 
-  response.json(savedUser);
+    const savedUser = await user.save();
+
+    response.json(savedUser);
+
+  } catch(error) {
+    response.status(400).send('Username must be unique');
+  }
 });
 
 
