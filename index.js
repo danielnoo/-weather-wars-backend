9da38a2843f21ca14ce787ae816ce944b66a7f0c
danielnoo <dannynoo@gmail.com>
2021-11-30 12:27:37 -0500

    installed node-cron to run every day at 3pm

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require("http");
 const config = require("./utils/config");
 const logger = require("./utils/logger");
 const getAndPostData = require('./modules/getAndPostData')
-
+const cron = require("node-cron");
 
 const server = http.createServer(app);
 
@@ -12,7 +12,18 @@ server.listen(config.PORT, () => {
 });
 
 
-// getAndPostData()
+
+// Run cron-job every day at 3pm - gathers weather API data and saves to DB
+
+cron.schedule("0 0 15 * * *", () => {
+  logger.info('running cron-job - getting weather and forecasts and saving to DB');
+
+  getAndPostData();
+}, {
+   scheduled: true,
+   timezone: "America/Toronto"
+});
+
 
 
 
